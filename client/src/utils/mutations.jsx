13e2fe086feac2fe9dd09d3861c8f4a8efe190c7
brennaveir <brennaveir@hotmail.com>
2023-09-08 15:37:24 -0400

    added mutations

---
+++ client/src/utils/mutations.jsx
@@ -0,0 +1,56 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_BIT = gql`
+  mutation addBit($bitText: String!) {
+    addBit(bitText: $bitText) {
+      _id
+      bitText
+      bitAuthor
+      createdAt
+      comments {
+        _id
+        commentText
+      }
+    }
+  }
+`;
+
+export const ADD_COMMENT = gql`
+  mutation addComment($bitId: ID!, $commentText: String!) {
+    addComment(bitId: $bitId, commentText: $commentText) {
+      _id
+      bitText
+      bitAuthor
+      createdAt
+      comments {
+        _id
+        commentText
+        createdAt
+      }
+    }
+  }
+`;
