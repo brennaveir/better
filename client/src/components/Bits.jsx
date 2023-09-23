c88ed4a9cf6ce610ee2b95b41251fdb791ef434c
brennaveir <brennaveir@hotmail.com>
2023-09-23 18:21:06 -0400

    fixed bug that caused profile to not come up when clicking on username

--- client/src/components/Bits.jsx
+++ client/src/components/Bits.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import Card from 'react-bootstrap/Card';
 
 
 const Bits = ({
@@ -15,43 +15,76 @@ const Bits = ({
   
 
   return (
-    <div>
-      {showTitle && <h3>{title}</h3>}
-      {bits &&
-        bits.map((bit) => (
-          <div key={bit._id} className="card mb-3">
-            <h4 className="card-header bg-primary text-light p-2 m-0">
-              {showUsername ? (
-                <Link
-                  className="text-light"
-                  to={`/profiles/${bit.bitAuthor}`}
-                >
-                  {bit.bitAuthor} <br />
-                  <span style={{ fontSize: '1rem' }}>
-                    posted this bit on {bit.createdAt}
-                  </span>
-                </Link>
-              ) : (
-                <>
-                  <span style={{ fontSize: '1rem' }}>
-                    You posted this bit on {bit.createdAt}
-                  </span>
-                </>
-              )}
-            </h4>
-            <div className="card-body bg-light p-2">
-              <p>{bit.bitText}</p>
-            </div>
-            <Link
-              className="btn btn-primary btn-block btn-squared"
-              to={`/bits/${bit._id}`}
-            >
-              Join the discussion on this bit.
-            </Link>
-          </div>
-        ))}
-    </div>
-  );
-};
+     
+      <Card style={{ width: '18rem' }}>
+         {showTitle && <h3>{title}</h3>}
+    {bits &&
+    bits.map((bit) => (
+    <Card.Body key={bit.id}> 
+      {showUsername ? (
+<Card.Title>
+        <Link to={`/profiles/${bit.bitAuthor}`}>{bit.bitAuthor}</Link>
+        <Card.Subtitle className="mb-2 text-muted">{bit.createdAt}</Card.Subtitle>
+        </Card.Title>
+      
+      ) : (
+        <>
+        <Link to={`/profiles/${bit.bitAuthor}`}>You</Link>
+        
+        </>
+      )}
+<Card.Text>
+        {bit.bitText}
+      </Card.Text>
+      <Link to={`/bits/${bit._id}`}>💬</Link>
+      
+    </Card.Body>
+  ))}
+  </Card>
+    
+    
+    
+  )
+      }
+  
+    // <div>
+    //   {showTitle && <h3>{title}</h3>}
+    //   {bits &&
+    //     bits.map((bit) => (
+    //       <div key={bit._id} className="card mb-3">
+    //         <h4 className="card-header bg-primary text-light p-2 m-0">
+    //           {showUsername ? (
+    //             <Link
+    //               className="text-light"
+    //               to={`/profiles/${bit.bitAuthor}`}
+    //             >
+    //               {bit.bitAuthor} <br />
+    //               <span style={{ fontSize: '1rem' }}>
+    //                 posted this bit on {bit.createdAt}
+    //               </span>
+    //             </Link>
+    //           ) : (
+    //             <>
+    //               <span style={{ fontSize: '1rem' }}>
+    //                 You posted this bit on {bit.createdAt}
+    //               </span>
+    //             </>
+    //           )}
+    //         </h4>
+    //         <div className="card-body bg-light p-2">
+    //           <p>{bit.bitText}</p>
+    //         </div>
+    //         <Link
+    //           className="btn btn-primary btn-block btn-squared"
+    //           to={`/bits/${bit._id}`}
+    //         >
+    //           Join the discussion on this bit.
+    //         </Link>
+    //       </div>
+    //     ))}
+    // </div>
+    
+ 
+
 
 export default Bits;
